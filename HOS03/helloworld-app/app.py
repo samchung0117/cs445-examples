aca3b3303c4c2281f14703cbb1ba6740a7c8f8fc
Sam Chung <chungsam@cityu.edu>
2025-04-19 00:42:31 +0000

    HOS03

--- HOS03/helloworld-app/app.py
+++ HOS03/helloworld-app/app.py
@@ -6,50 +6,52 @@ database = {}
 app = Flask(__name__)
 
 @app.route("/")
+
 def index():
     return "Hello World!"
 
-@app.route('/students', methods=['POST'])
-def post_student_details():
+@app.route('/students', methods=['POST']) 
+def post_students_details(): 
+    try: 
+        data = request.json 
+        dict_json = json.loads(json.dumps(data)) 
+        database[dict_json["name"]] = dict_json["age"] 
+        return 'Success', 200 
+    except Exception as e: 
+        print("Error during saving object ", e) 
+        return 'Failed', 400 
+
+@app.route('/students/<Student_name>', methods=['GET']) 
+def get_students_details(Student_name): 
     try:
-        data = request.json
-        dict_json = json.loads(json.dumps(data))
-        database[dict_json["name"]] = dict_json["age"]
-        return 'Success', 200
-    except Exception as e:
-        print('Error during saving object', e)
-        return 'Failed', 400
-    
-@app.route('/students/<student_name>')
-def get_student_details(Student_name):
-    try:
-        name = database[Student_name]
-        if name == None:
-            return 'Record not found', 404
+        name = database[Student_name] 
+        if name == None: 
+            return 'Record Not Found', 404 
         else:
-            return 'Record found' + Student_name + 'age is ' + str(name['age']), 200
-    except KeyError:
-        return 'Record Not Found', 404
+            return 'Record Found ' + Student_name + ' age is ' + str(name), 200 
+    except KeyError: 
+        return 'Record Not Found', 404 
+    
+@app.route('/students', methods=['PUT']) 
+def put_students_details(): 
+    try: 
+        data = request.json 
+        dict_json = json.loads(json.dumps(data)) 
+        database[dict_json["name"]] = dict_json["age"] 
+        return 'Success', 200 
+    except Exception as e: 
+        print("Error during saving object ", e) 
+        return 'Failed', 400 
 
-@app.route('/students', methods=['PUT'])
-def put_student_details():
+@app.route('/students/<Student_name>', methods=['DELETE']) 
+def delete_students_details(Student_name): 
     try:
-        data = request.json
-        dict_json = json.loads(json.dumps(data))
-        database[dict_json['name']] = dict_json['age']
-        return 'Success', 200
-    except Exception as e:
-        print('Error during saving object: ' + str(e))
-        return 'Failed', 400
-
-@app.route('/students/<student_name>', methods=['DELETE'])
-def delete_student_details(student_name):
-    try:
-        name = database[student_name]
-        database.pop(student_name)
-        return 'Record deleted successfully', 200
+        name = database[Student_name] 
+        database.pop(Student_name) 
+        return 'Record deleted successfully', 200 
     except KeyError:
-        return 'Record not found', 404
-    except Exception as e:
-        print("Error while removing record: {}".format(e))
-        return 'Error while removing record', 400
+        return 'Record Not Found', 404 
+    except Exception as e: 
+        print("Error while removing record ", e) 
+        return 'Error while removing record', 400 
+    
